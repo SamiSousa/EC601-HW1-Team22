d3c353962de496e12502c2700a374e92b0e26579
david179 <31712615+david179@users.noreply.github.com>
2017-09-14 16:22:46 -0300

    Update usage.cpp

--- src/usage.cpp
+++ src/usage.cpp
@@ -18,20 +18,6 @@ int main()
 
 	VideoCapture video = openVideo("video.avi");
 
-	/*Mat frame;
-	Mat frame2;
-	for(int i = 0;i < 4700; i++)
-	{
-		video >> frame;
-	}
-	video >> frame;
-	for (int i = 0; i < 30; i++)
-		video >> frame2;
-	video >> frame2;
-	imshow("frame1",frame);
-	imshow("frame2",frame2);
-	imshow("diff",frame-frame2);
-	waitKey(0);*/
 	playVideo(video,20);
 
 
