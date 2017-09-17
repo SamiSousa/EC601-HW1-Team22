551a225d5a387aa6acf8e73c09ae9c490cb31b78
david179 <31712615+david179@users.noreply.github.com>
2017-09-17 15:46:46 +0200

    Uploaded the code such that now it uses Davide's library

--- TianyiTest/src/main.cpp
+++ TianyiTest/src/main.cpp
@@ -16,41 +16,22 @@
 #include <math.h>
 #include <string>
 #include <unistd.h>
+#include "readVideoFunctions.hpp"
 
 using namespace std;
 using namespace cv;
-
-VideoCapture openVideo2(string filename);
-
-VideoCapture openVideo2(string filename)
-{
-	VideoCapture capture(filename);
-	if (!capture.isOpened())
-	{
-		throw "Error - could not open the video";
-	}
-
-	return capture;
-}
 
 
 static void help(char** argv)
 {
     // print a welcome message, and the OpenCV version
-    cout << "\nThis is a demo program shows how perspective transformation applied on an image, \n"
-         "Using OpenCV version " << CV_VERSION << endl;
+    cout << "\nThis is a demo program that cuts a square section from a video and applies to it a perspective transformation,  \n"
+         "using OpenCV version " << CV_VERSION << endl;
 
-    cout << "\nUsage:\n" << argv[0] << " [image_name -- Default ../data/right.jpg]\n" << endl;
+    cout << "\nUsage:\n" << argv[0] << " [video_name]\n" << endl;
 
-    cout << "\nHot keys: \n"
-         "\tESC, q - quit the program\n"
-         "\tr - change order of points to rotate transformation\n"
-         "\tc - delete selected points\n"
-         "\ti - change order of points to invers transformation \n"
-         "\nUse your mouse to select a point and move it to see transformation changes" << endl;
 }
 
-static void onMouse(int event, int x, int y, int, void*);
 Mat warping(Mat image, Size warped_image_size, vector< Point2f> srcPoints, vector< Point2f> dstPoints);
 
 String windowTitle = "Perspective Transformation Demo";
@@ -65,14 +46,13 @@ bool validation_needed = true;
 int main(int argc, char** argv)
 {
     help(argv);
-    CommandLineParser parser(argc, argv, "{@input| ../data/right.jpg |}");
+    CommandLineParser parser(argc, argv, "{@input}");
 
     string filename = parser.get<string>("@input");
 
-    VideoCapture video = openVideo2(filename);
+    VideoCapture video = openVideo(filename);
     Mat image;
-    // = imread( filename );
-    Mat frame, frame1;	/////////////
+
 
     // move a few frames ahead into the video
     Mat f;
@@ -104,75 +84,42 @@ int main(int argc, char** argv)
 
 	Mat H = findHomography(roi_corners, dst_corners); //get homography
 
-for(;;){//add a loop here so the program will deal with the frames one by one
+	for(;;){//add a loop here so the program will deal with the frames one by one
 
-    Mat original_image;
-	video >> original_image;
-	if (original_image.empty())
-	{
-		break;
-	}
-
-
-    namedWindow(windowTitle, WINDOW_NORMAL);
-    namedWindow("Warped Image", WINDOW_AUTOSIZE);
-    moveWindow("Warped Image", 20, 20);
-    moveWindow(windowTitle, 330, 20);
+		Mat original_image;
+		// get the next frame
+		video >> original_image;
+		if (original_image.empty())
+		{
+			break;
+		}
 
 
-	image = original_image.clone();
-	// draw the 4 points on the image
-	for ( int i = 0; i < 4; ++i )
-	{
-		line(image, roi_corners[i], roi_corners[(i + 1) % 4], Scalar(0, 0, 255), 2);
-		circle(image, roi_corners[i], 5, Scalar(0, 255, 0), 3);
-		putText(image, labels[i].c_str(), roi_corners[i], QT_FONT_NORMAL, 0.8, Scalar(255, 0, 0), 2);
-	}
-
-	imshow( windowTitle, image );
-
-	Mat warped_image;
-	warpPerspective(original_image, warped_image, H, warped_image_size); // do perspective transformation
-	imshow("Warped Image", warped_image);
-
-    waitKey( 20 );
+		namedWindow(windowTitle, WINDOW_NORMAL);
+		namedWindow("Warped Image", WINDOW_AUTOSIZE);
+		moveWindow("Warped Image", 20, 20);
+		moveWindow(windowTitle, 330, 20);
 
 
-}/////////
+		image = original_image.clone();
+		// draw the cut section on the image
+		for ( int i = 0; i < 4; ++i )
+		{
+			line(image, roi_corners[i], roi_corners[(i + 1) % 4], Scalar(0, 0, 255), 2);
+			circle(image, roi_corners[i], 5, Scalar(0, 255, 0), 3);
+			putText(image, labels[i].c_str(), roi_corners[i], QT_FONT_NORMAL, 0.8, Scalar(255, 0, 0), 2);
+		}
+
+		imshow( windowTitle, image );
+
+		// apply the perspective transformation
+		Mat warped_image;
+		warpPerspective(original_image, warped_image, H, warped_image_size);
+		imshow("Warped Image", warped_image);
+
+		waitKey( 20 );
+
+
+	}/////////
     return 0;
-}
-
-static void onMouse(int event, int x, int y, int, void*)
-{
-    // Action when left button is pressed
-    if (roi_corners.size() == 4)
-    {
-        for (int i = 0; i < 4; ++i)
-        {
-            if ((event == EVENT_LBUTTONDOWN) & ((abs(roi_corners[i].x - x) < 10)) & (abs(roi_corners[i].y - y) < 10))
-            {
-                selected_corner_index = i;
-                dragging = true;
-            }
-        }
-    }
-    else if ( event == EVENT_LBUTTONDOWN )
-    {
-        roi_corners.push_back( Point2f( (float) x, (float) y ) );
-        validation_needed = true;
-    }
-
-    // Action when left button is released
-    if (event == EVENT_LBUTTONUP)
-    {
-        dragging = false;
-    }
-
-    // Action when left button is pressed and mouse has moved over the window
-    if ((event == EVENT_MOUSEMOVE) && dragging)
-    {
-        roi_corners[selected_corner_index].x = (float) x;
-        roi_corners[selected_corner_index].y = (float) y;
-        validation_needed = true;
-    }
 }
